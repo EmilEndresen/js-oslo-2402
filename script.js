f968d3d77caba0d240c6ce9b3a6109202e01fe41
Emil Johan Endresen <emil@jobloop.no>
2025-02-24 10:08:32 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Hello World")
+
+// var x = 5 Vi bruker ikke var lengre
+
+const name = "Emil"
+
+let firstName = "Emil" // String
+let age = 30 // Integer/Number
+let exampleFloat = 30.2 // Float
+let fromBergen = true // Boolean
+let exampleArray = ["Mazda", "Opel", "Saab", "Mitsubishi"] // Array
+let myObject = {
+   firstName: "Torleif",
+   lastName: "Leiknes",
+   age: 67,
+   eMail: "torleif@leiknes.no"
+}
+
+console.log(myObject)
+
+console.log(exampleArray)
+
+let x = 11
+let y = 4
+
+x++
+console.log(x)
+
+let z = x + y
+console.log(z)
